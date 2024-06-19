19a624461e9839d4600cf5c2408a501567c82925
DT233974 <Vinayak.Gawade@sscinc.com>
2024-06-19 19:25:06 +0530

    First Commit

---
+++ src/main/java/com/rocksddservice/rocksddservice/RocksddserviceApplication.java
@@ -0,0 +1,13 @@
+package com.rocksddservice.rocksddservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocksddserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocksddserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocksddservice/rocksddservice/RocksddserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocksddservice.rocksddservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocksddserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
