031113232c5535057159d455ece7ffc25883360d
DT233974 <Vinayak.Gawade@sscinc.com>
2024-06-20 12:26:48 +0530

    Updating: Renaming application main method class

--- src/main/java/com/rocksddservice/rocksddservice/RocksDDServiceApplication.java
+++ src/main/java/com/rocksddservice/rocksddservice/RocksDDServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RocksddserviceApplication {
+public class RocksDDServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RocksddserviceApplication.class, args);
+		SpringApplication.run(RocksDDServiceApplication.class, args);
 	}
 
 }
--- src/test/java/com/rocksddservice/rocksddservice/RocksDDServiceApplicationTests.java
+++ src/test/java/com/rocksddservice/rocksddservice/RocksDDServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RocksddserviceApplicationTests {
+class RocksDDServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
